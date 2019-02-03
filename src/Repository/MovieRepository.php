5c1fdd54178ea2ddefff878cfaccf3c6570fbeb2
Jeremy Gaultier <contact@webmezenc.com>
2019-02-03 16:41:31 +0100

    tech(repository): add findbestmovie

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Movie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -17,5 +18,19 @@ class MovieRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Movie::class);
+    }
+
+    public function findBestMovie(): ?Movie
+    {
+        $qb = $this->createQueryBuilder('m');
+
+        return $qb ->innerJoin('m.users', 'u')
+                   ->groupBy('m')
+                   ->orderBy(
+                       $qb->expr()->count('m'), 'DESC'
+                    )
+                    ->setMaxResults(1)
+                    ->getQuery()
+                    ->getOneOrNullResult();
     }
 }
